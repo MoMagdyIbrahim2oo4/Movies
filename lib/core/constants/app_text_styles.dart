b29ec547fc9912d883d0c179331590f51ceb076e
Andrew Youssef Eshak <androwyoussefeshak@email.com>
2026-09-04 03:55:06 +0300

    Update authentication module: UI, form validation, and styling

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -3,6 +3,19 @@ import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movies/core/constants/app_colors.dart';
 class AppTextStyles{
+
+  static final TextStyle regular14White=GoogleFonts.roboto(
+    color: AppColors.white ,
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular14Amber=GoogleFonts.roboto(
+    color: AppColors.amber,
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
   static final TextStyle regular16Amber=GoogleFonts.roboto(
     color: AppColors.amber,
     fontSize: 16.sp,
@@ -11,6 +24,12 @@ class AppTextStyles{
 
   static final TextStyle regular16White=GoogleFonts.roboto(
     color: AppColors.white,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular16DarkGray=GoogleFonts.roboto(
+    color: AppColors.darkGray ,
     fontSize: 16.sp,
     fontWeight: FontWeight.w400,
   );
