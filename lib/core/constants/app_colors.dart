de35949c2ccaf9e52fc9a0c7f7ed26f989381764
magdy <wfaa.magdy2014@gmail.com>
2026-09-07 16:58:56 -0700

    finish browse ui

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -7,4 +7,5 @@ class AppColors {
   static const Color darkCharcoalGray=Color(0xFF282A28);
   static const Color white=Color(0xFFFFFFFF);
   static const Color crimson=Color(0xFFE82626);
+  static const Color black=Color(0xFF0000000);
 }
