26dc5556c5cc10d99e80e86aa1aa9c24e66a6768
Tarek Moawad <tmoawad26@gmail.com>
2026-09-24 06:08:20 -0700

    Handling exception using apiresult and apicall

--- lib/features/movie_details/presentation/cubit/movie_sugestion_cubit.dart
+++ lib/features/movie_details/presentation/cubit/movie_sugestion_cubit.dart
@@ -1,27 +1,20 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/features/movie_details/data/repos/movie_suggestion_repo.dart';
 import 'package:movies/features/movie_details/presentation/cubit/movie_sugestions_state.dart';
 
-
 class MovieSugestionsCubit extends Cubit<MovieSugestionsState> {
   final MovieSuggestionRepo _movieSuggestionRepo;
-  MovieSugestionsCubit(this._movieSuggestionRepo) : super(MovieSugestionsInitial());
-  Future<void>loadSuggestions(String movieID) async {
+
+  MovieSugestionsCubit(this._movieSuggestionRepo)
+      : super(MovieSugestionsInitial());
+
+  Future<void> loadSuggestions(String movieID) async {
     emit(MovieSugestionsLoading());
-    try{
-      final data=await _movieSuggestionRepo.getSugestions(movieID);
-      emit(MovieSugestionsSuccess(data));
-    }
-    catch (error){
-      String errorMessage;
-      if(error is DioException){
-        errorMessage=error.message??'Dio Error occured';
-      }
-      else{
-        errorMessage=error.toString();
-      }
-      emit(MovieSugestionsError(errorMessage));
-    }
+    final result = await _movieSuggestionRepo.getSugestions(movieID);
+    if (isClosed) return;
+    result.when(
+      success: (movies) => emit(MovieSugestionsSuccess(movies)),
+      failure: (errorMessage) => emit(MovieSugestionsError(errorMessage)),
+    );
   }
 }
--- lib/features/search/data/repos/search_repo_imp.dart
+++ lib/features/search/data/repos/search_repo_imp.dart
@@ -1,12 +1,15 @@
 import 'package:movies/core/models/movie_model.dart';
+import 'package:movies/core/network/api_call.dart';
+import 'package:movies/core/network/api_result.dart';
 import 'package:movies/features/search/data/datasources/search_remote_data_source.dart';
 import 'package:movies/features/search/data/repos/search_repo.dart';
 
-class SearchRepoImp implements SearchRepo{
+class SearchRepoImp implements SearchRepo {
   final SearchRemoteDataSource _searchRemoteDataSource;
   SearchRepoImp(this._searchRemoteDataSource);
+
   @override
   Future<List<Movie>> search(String queryItem) async {
-    return await _searchRemoteDataSource.search(queryItem);
+    return await  _searchRemoteDataSource.search(queryItem);
   }
 }
